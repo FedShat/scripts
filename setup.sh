1b5c569c4029f2cae41d06773af48023f12c32a5
FedorShatokhin2005 <groza01i@gmail.com>
2020-04-24 10:58:08 +0500

    setup: Install oh-my-zsh, powerlevel10k
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- setup.sh
+++ setup.sh
@@ -10,6 +10,12 @@ git config --global user.name "FedorShatokhin2005"
 git config --global user.email "groza01i@gmail.com"
 git config --global credential.helper store
 
+# Installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installing powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g" ~/.zshrc
 
 # Clone kernel build dependencies
 mkdir ~/build
