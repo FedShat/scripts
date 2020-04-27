ccd6bc084b5872fd0e93247177c39621bd526140
FedorShatokhin2005 <groza01i@gmail.com>
2020-04-27 09:20:29 +0500

    Initial script for importing wireguard
    * credits @zx2c4
    
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

---
+++ wireguard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+USER_AGENT="WireGuard-AndroidROMBuild/0.3 ($(uname -a))"
+
+read -p 'Version: ' VERSION
+
+rm -rf net/wireguard
+mkdir -p net/wireguard
+curl -A "$USER_AGENT" -LsS --connect-timeout 30 "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$VERSION.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "wireguard-linux-compat-$VERSION/src"
+sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
+
