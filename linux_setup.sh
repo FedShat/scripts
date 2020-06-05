869b6a396ba505ee39b43db513bb037e6ee62194
FedorShatokhin2005 <groza01i@gmail.com>
2020-06-05 16:51:59 +0500

    setup: install zsh only in linux_setup
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- linux_setup.sh
+++ linux_setup.sh
@@ -7,7 +7,11 @@ git config --global credential.helper store
 git config --global commit.gpgsign true
 git config --global user.signingkey "688C4735813B4124"
 
-echo 'alias regen="make ARCH=arm64 platina_defconfig; mv .config arch/arm64/configs/platina_defconfig"' >> ~/.bashrc
+echo 'alias regen="make ARCH=arm64 platina_defconfig; mv .config arch/arm64/configs/platina_defconfig"' >> ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g" ~/.zshrc
 
 git clone --depth=1 https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
 git clone --depth=1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
